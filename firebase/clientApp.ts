c661d5d363d9bab0ba61cd868c45b09397c9e19d
Ohm-Rajpal <137588636+Ohm-Rajpal@users.noreply.github.com>
2024-08-02 19:07:20 -0700

    Initialized firebase

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,3 +1,8 @@
+import firebase from "@firebase/app-compat";
+import {initializeApp} from "@firebase/app";
+import {getAnalytics} from "@firebase/analytics";
+import {getFirestore} from "@firebase/firestore";
+
 const firebaseConfig = {
     apiKey: process.env["NEXT_PUBLIC_FIREBASE_API_KEY "],
     authDomain: process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN "],
@@ -7,3 +12,10 @@ const firebaseConfig = {
     appId: process.env["NEXT_PUBLIC_FIREBASE_APP_ID "],
     measurementId: process.env["NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID "]
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
